6f04fc5f30ad3e8cc040eaf149636777b7da6b43
George Dobbs <george.dobbs@gmail.com>
2023-01-12 11:06:42 -0500

    index is xtrk index + segment # times 0.1

--- elevations.py
+++ elevations.py
@@ -16,21 +16,25 @@ regex=r'  '
 with open(path.expanduser(in_file),encoding='windows-1252') as f:
   flag=False
   track_type=idx=None
+  sub=0
   for in_line in f.readlines():
     line=re.sub(regex,r' ',in_line)
     if flag:
       assert line[0]=='\t'
       fields=line[1:].split(' ')
       if fields[0]=='T4':
-        elevs=[float(fields[a]) for a in (9,13)]
-      recs[idx]=[track_type]+elevs
+        elevs=[round(float(fields[a]),3) for a in (9,13)]
+        recs[idx+(.1*sub)]=[track_type]+elevs
+        sub=sub+1
       if fields[0]=='END$SEGS\n':
         flag=False
+        sub=0
     else:
       fields=line.strip().split(' ')
       if fields[0]in ['STRAIGHT','CURVE','TURNOUT']:
         track_type=fields[0]
-        idx=int(fields[1])
+        idx=float(fields[1])
+        sub=0 # sub index
         flag=True
 
 df =pd.DataFrame(recs).T
